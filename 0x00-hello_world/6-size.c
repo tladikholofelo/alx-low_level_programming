cfe10a5fc0259d5e8753eaa93034a1d9d2ab24c4
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-09-08 18:37:00 -0700

    Fixed spelling error "an int" instead of "a int"

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float floatType;
 	/*sizeof evaluates the size of a variable*/
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
