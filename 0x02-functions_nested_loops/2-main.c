37edbad20bee2208623bf666adca5cde304f752e
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-09-14 13:11:03 -0700

    Prints the n times table, starting with 0

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
- * main - entry point
- * print_alphabet_x10 - prints 10 times the alphabet
+ * main - check the code.
  *
  * Return: Always 0.
  *
  */
-
 int main(void)
-
 {
-
 	print_alphabet_x10();
 	return (0);
-
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,26 @@
 /**
  * print_alphabet_x10 - prints the alphabet 10 times
  *
- * Return: 0.
+ * Return: the result.
  */
 
 void print_alphabet_x10(void)
 {
 	int j = 0;
 
-	while (j < 0)
+	while (j < 10)
 	{
 		char abc = 'a';
 
-		while  (j <= 'z')
-		{
-			_putchar(abc);
-			abc++;
-		}
+		while (j <= 'z')
+	{
+		_putchar(abc);
+
+		abc++;
 	}
+	}
+
 	_putchar('\n');
+
 	j++;
 }
