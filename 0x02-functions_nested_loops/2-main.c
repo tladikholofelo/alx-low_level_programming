6ecd0e238e103d897e8223daa6b9d40f1eb84a41
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-09-14 02:11:56 -0700

    Prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+
+	print_alphabet_x10();
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 0)
+	{
+		char abc = 'a';
+
+		while  (j <= 'z')
+		{
+			_putchar(abc);
+			abc++;
+		}
+	}
+	_putchar('\n');
+	j++;
+}
