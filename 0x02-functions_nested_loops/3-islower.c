a2d4bced2066ab4f60556dad8b45a0491e51c9f7
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-09-14 03:07:47 -0700

    Checks for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase characters
+ *
+ * @c: prints to stdout
+ *
+ * Return: 0.
+ *
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
