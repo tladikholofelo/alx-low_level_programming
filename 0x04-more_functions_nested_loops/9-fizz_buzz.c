f8c8cdcc7ad198191de521cd4cb3ed0f190baf8d
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-09-16 03:46:55 -0700

    Prints the numbers from 1 to 100, "The Fizz-Buzz test"

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,27 +15,18 @@ int main(void)
 {
 	int i;
 
-	i = 1;
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0 && i % 5 == 0))
-		{
+		if (i == 100)
+			printf("Buzz");
+		else if (((i % 3) == 0) && ((i % 5) == 0))
 			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-		}
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
-		{
 			printf("%d ", i);
-		}
-
-		i++;
 	}
 	printf("\n");
 
