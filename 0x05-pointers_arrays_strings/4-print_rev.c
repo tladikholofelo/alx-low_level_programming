cfdc7f640da4ffb40faaf084238d2e499434deb8
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-09-20 02:42:40 -0700

    Prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: the string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
