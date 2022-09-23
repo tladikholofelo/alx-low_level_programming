80d1ed527fa0d8db20b420c04b3ada626c198e05
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-09-23 01:15:34 -0700

    Encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -19,15 +19,15 @@
 char *leet(char *s)
 {
 	int i, count = 0;
-	int lower_case[] = {97, 101, 111, 116, 108};
-	int upper_case[] = {65, 69, 79, 84, 76};
+	int lower_alpha[] = {97, 101, 111, 116, 108};
+	int upper_alpha[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
+			if (*(s + count) == lower_alpha[i] || *(s + count) == upper_alpha[i])
 			{
 				*(s + count) = numbers[i];
 				break;
