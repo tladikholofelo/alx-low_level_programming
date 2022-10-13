915cae1e37c147ef6ee534b46ab44003d8ae1bed
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-10-12 19:18:47 -0700

    Main function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+
+/**
+ * main - program that performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0. (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char operator;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operator = *argv[2];
+	if ((operator == '/' || operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = f(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
