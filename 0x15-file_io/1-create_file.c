34598a5da5074f7bc319205f14318ebdc0edb0f2
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-10-31 19:03:58 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 			alpha++;
 
 		file_write = write(file, text_content, alpha);
-		if (write_status == -1)
+		if (file_write == -1)
 			return (-1);
 	}
 
