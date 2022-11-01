4f5cf53f725077fb94821aeee42e9dea80a7a5e6
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-10-31 19:58:37 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 void error_file(int file_from, int file_to, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif /* _MAIN_H_ */
