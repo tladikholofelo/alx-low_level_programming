57c02714a0a2c182b535d91250c1853d62b07489
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-10-31 18:58:21 -0700

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure.
+ * If the file already exists, truncate it.
+ * If filename is NULL return -1.
+ * If text_content is NULL create an empty file.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, file_write, alpha = 0;
+
+	if (!filename)
+		return (-1);
+
+	/* if the file already exists, truncate it */
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		while (text_content[alpha] != '\0')
+			alpha++;
+
+		file_write = write(file, text_content, alpha);
+		if (write_status == -1)
+			return (-1);
+	}
+
+	close(file);
+	return (1);
+}
+
+
