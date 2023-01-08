aba9ea987069067d0260165b3a31bd5277655cec
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2023-01-08 15:35:40 -0800

    Add 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: the key
+ * @size: the size of the array of the hash table
+ *
+ * Description: This function should use the hash_djb2 function
+ *
+ * Return: Index at which the key/value pair should be stored
+ * in the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
